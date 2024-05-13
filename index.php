a71bfa3a79349afcff83be092dbef86e242f7e9a
rodrigomirandaa <digoracm@gmail.com>
2024-05-13 20:32:23 -0300

    feat: banner e card

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 <?php include('components/head.php'); ?>
 <?php include('components/navbar.php'); ?>
 <?php include('components/banner.php'); ?>
+<?php include('components/cards.php'); ?>
+
 <?php require('components/footer.php'); ?>
+
 
 
 
