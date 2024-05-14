7dd84d32c61be51e85a8ece72ffe3e8bcbc6a1bd
rodrigomirandaa <digoracm@gmail.com>
2024-05-14 18:35:04 -0300

    fixnavbar

--- components/navbar.php
+++ components/navbar.php
@@ -4,18 +4,18 @@
             <img src=static/logo.svg alt="Logo" width="70" height="70">
         </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon" style="filter: invert(100%);"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link text-dark fw-regular" href="#">Início</a>
+                        <a class="nav-link text-dark fw-regular" href="index.php">Início</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-dark fw-regular" href="#">Sobre</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-dark fw-regular" href="#">Cadastrar clínicas</a>
+                        <a class="nav-link text-dark fw-regular" href="CadastrarClinicas.php">Cadastrar clínicas</a>
                     </li>
                 </ul>
             </div>
