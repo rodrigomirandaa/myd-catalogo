78e27b170fe6f595ddcb13494e4349c6e3b87473
joaomoraiss <joaovictorrmpework@outlook.com>
2024-05-21 18:54:32 -0300

    seção sobre

--- components/navbar.php
+++ components/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-light">
         <div class="container">
-        <a class="navbar-brand" href="index.php">
-            <img src=static/logo.svg alt="Logo" width="70" height="70">
+        <a class="navbar-brand" href="/index.php">
+            <img src=/static/logo.svg alt="Logo" width="70" height="70">
         </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon" style="filter: invert(100%);"></span>
@@ -9,10 +9,10 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link text-dark fw-regular" href="index.php">Início</a>
+                        <a class="nav-link text-dark fw-regular" href="/index.php">Início</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-dark fw-regular" href="#">Sobre</a>
+                        <a class="nav-link text-dark fw-regular" href="/components/sobre.php">Sobre</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-dark fw-regular" href="CadastrarClinicas.php">Cadastrar clínicas</a>
---
+++ components/sobre.php
@@ -0,0 +1,25 @@
+<?php include('../components/head.php'); ?>
+<?php include('../components/navbar.php'); ?>
+
+<section style="display: flex; flex-direction:column ;align-items:center; " id="sobre">
+  <h1>Make Your Day</h1>
+  <p  style="font-size: 1.7rem; text-align:center; margin: 0 3.2rem 2.5rem 3.2rem;">
+    Bem-vindo ao nosso site! Somos uma empresa dedicada a fornecer os melhores serviços
+    e produtos para nossos clientes. Nossa missão é entregar qualidade e satisfação em
+    tudo o que fazemos. Através de extensas pesquisas, nossa equipe está pronta para
+    atender suas necessidades e superar suas expectativas.
+  </p>
+  <p style="font-size: 1.7rem; text-align:center; margin:0 3.2rem 2.5rem 3.2rem;">
+    Nossa história começou há muitos anos, quando identificamos uma necessidade no mercado
+    por serviços de alta qualidade e atendimento personalizado. Desde então, temos crescido
+    e evoluído, sempre mantendo nosso compromisso com a excelência.
+  </p>
+  <p style="font-size: 1.7rem; text-align:center; margin:0 3.2rem 2.1rem 3.2rem;">
+    Acreditamos que nosso sucesso é resultado do trabalho duro, da paixão pelo que fazemos
+    e do compromisso com nossos valores. Convidamos você a explorar nosso site, conhecer
+    mais sobre nossos serviços e entrar em contato conosco para qualquer dúvida ou informação
+    adicional. Estamos aqui para ajudar!
+  </p>
+</section>
+
+<?php include('../components/footer.php'); ?>
