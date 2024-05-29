fbb3042967af7e42a52ba7734c174327d8396bde
Ana Cecília Simões Chalegre <130169684+ceciliasimoes@users.noreply.github.com>
2024-05-28 22:09:30 -0300

    Update head.php

--- components/head.php
+++ components/head.php
@@ -7,11 +7,9 @@
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
-
-    <link href="styles/styles.css" rel="stylesheet">
+    <link href="styles/style.css" rel="stylesheet">
 </head>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
 </html>
