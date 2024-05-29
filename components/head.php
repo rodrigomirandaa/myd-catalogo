2dbaa2dd2d3c7e1cb1ec450c602546eb3e0bce15
AC <acsimoes446@gmail.com>
2024-05-28 23:44:25 -0300

    fix: Corrigindo variaveis incorretas do css e localizacao incorreta da pagina Sobre

--- components/head.php
+++ components/head.php
@@ -6,13 +6,8 @@
     <title>Make Your Day</title>
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="icon" type="image/x-icon" href="../static/logo.svg">
 
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
-
-    <link href="../styles/style.css" rel="stylesheet">
+    <link href="styles/style.css" rel="stylesheet">
 </head>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
--- sobre.php
+++ sobre.php
@@ -1,5 +1,5 @@
-<?php include('../components/head.php'); ?>
-<?php include('../components/navbar.php'); ?>
+<?php include('components/head.php'); ?>
+<?php include('components/navbar.php'); ?>
 
 <section style="display: flex; flex-direction:column ;align-items:center; " id="sobre">
   <h1 style="margin-top: 0.5rem;">Make Your Day</h1>
@@ -22,33 +22,33 @@
   </p>
 </section>
 <section id="team" style="display: flex; flex-wrap: wrap; justify-content: center; gap: 2rem; padding: 2rem;">
-  <div class="card">
-    <img src="../static/joaoMorais.jpg" alt="João Morais">
+  <div class="cardSobre">
+    <img src="static/joaoMorais.jpg" alt="João Morais">
     <h3>João Morais</h3>
     <p><a href="https://www.linkedin.com/in/jo%C3%A3o-morais-4b231a269/" target="_blank">LinkedIn</a> | <a href="https://github.com/joaomoraiss" target="_blank">GitHub</a></p>
   </div>
-  <div class="card">
-    <img src="../static/rodrigoMiranda.jpg" alt="Membro 2">
+  <div class="cardSobre">
+    <img src="static/rodrigoMiranda.jpg" alt="Membro 2">
     <h3>Rodrigo Miranda</h3>
     <p><a href="https://www.linkedin.com/in/rodrigomirandaa/" target="_blank">LinkedIn</a> | <a href="https://github.com/rodrigomirandaa/" target="_blank">GitHub</a></p>
   </div>
-  <div class="card">
-    <img src="../static/matheusAntonio.jpg" alt="Membro 3">
+  <div class="cardSobre">
+    <img src="static/matheusAntonio.jpg" alt="Membro 3">
     <h3>Matheus Antonio</h3>
     <p><a href="https://br.linkedin.com/in/matheus-antonio-399346272" target="_blank">LinkedIn</a> | <a href="https://github.com/matheusantonio156" target="_blank">GitHub</a></p>
   </div>
-  <div class="card">
-    <img src="../static/gustavoFelipe.jpg" alt="Membro 4">
+  <div class="cardSobre">
+    <img src="static/gustavoFelipe.jpg" alt="Membro 4">
     <h3>Gustavo Felipe</h3>
     <p><a href="https://www.linkedin.com/in/gustavo-felipe-1382a426b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">LinkedIn</a> | <a href="https://github.com/GustavoFelips" target="_blank">GitHub</a></p>
   </div>
-  <div class="card">
-    <img src="../static/victorCosta.jpg" alt="Membro 5">
+  <div class="cardSobre">
+    <img src="static/victorCosta.jpg" alt="Membro 5">
     <h3>Victor Costa</h3>
     <p><a href="https://www.linkedin.com/in/victor-costa-14295a1b1?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">LinkedIn</a> | <a href="https://github.com/victorcosta213" target="_blank">GitHub</a></p>
   </div>
-  <div class="card">
-    <img src="../static/anaCecilia.jpg" alt="Membro 6">
+  <div class="cardSobre">
+    <img src="static/anaCecilia.jpg" alt="Membro 6">
     <h3>Ana Cecilia</h3>
     <p><a href="https://www.linkedin.com/in/ana-cecilia-simoes-chalegre/" target="_blank">LinkedIn</a> | <a href="https://github.com/ceciliasimoes" target="_blank">GitHub</a></p>
   </div>
