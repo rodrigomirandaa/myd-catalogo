f38825d7e5975ba1a0752ac5530cb11c2d900b16
rodrigomirandaa <digoracm@gmail.com>
2024-06-08 09:29:58 -0300

    fix: add clinicas

--- components/head.php
+++ components/head.php
@@ -12,4 +12,6 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
+<link rel="icon" href="/static/logo.svg" type="image/png">
+
 </html>
